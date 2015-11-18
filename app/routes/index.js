32d7cdbb22fd9051dcae51f0872990b10bcff251
Erik Mellum <emellum@mail.csuchico.edu>
2015-11-17 16:11:48 -0800

    Mac

--- app/routes/index.js
+++ app/routes/index.js
@@ -29,7 +29,7 @@ router.post('/custom-f6', function(req, res, next) {
         res.set(
           'Content-Type', 'application/zip'
         );
-        res.sendfile('custom-f6-'+uniq+'.zip', {root: 'public/assets'}, cleanup)
+        res.sendfile('custom-f6-'+uniq+'.zip', {root: 'assets'}, cleanup)
       });
 
       //error almost always means the src path didn't exist
